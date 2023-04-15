b1a59263f6b4bbbc62ccd1054924bf210bf0bcaf
CDS-admin1 <csouthon44@gmail.com>
2023-04-15 17:01:12 +0100

    Creating projectcards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import Tilt from 'react-parallax-tilt';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
+    // revise projects
+    return (
+      <motion.div>
+        test
+      </motion.div>
+    )
+}
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+       <motion.div variants={textVariant()}>
+        {/* revise text */}
+      <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Here are my projects, showing my skills and experiences. Including live demo links and repository links.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard 
+          key={`project-${index}`}
+          index={index}
+          {...project}
+          />
+        ))}
+
+      </div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
