fee736c222d8c5f3f300de60c56d457536e5514d
CDS-admin1 <csouthon44@gmail.com>
2023-04-15 12:51:03 +0100

    Creating ServiceCard in About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-// import Tilt from 'react-tilt';
+import Tilt from 'react-tilt';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="w-full green-pink-gradient p-[1] rounded-[20px] shadow-card">
+
+      </motion.div>
+    </Tilt>
+  )
+}
+
 
 const About = () => {
   return (
@@ -14,7 +26,8 @@ const About = () => {
       </motion.div>
 
       <motion.p
-      variants={fadeIn("", "", 0.1, 1)}>
+      variants={fadeIn("", "", 0.1, 1)}
+      className="mt-4 text-secondarytext-[17px] max-w-3xl leading-[30px]">
         {/* revise this text */}
         I'm a Front End Web Developer Student based in the Isle of Wight, UK.
         Before becoming a student, I was a chef for 20 years until 
@@ -24,6 +37,13 @@ const About = () => {
         I'm enthusiastic when it comes to learning something new and I love a challenge and problem solving, 
         I don't give up (unless there's no more coffee!).
       </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {/* services for card found in index.js */}
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   )
 }
