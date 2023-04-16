46680a62c2352543c491db16936780f5df189143
CDS-admin1 <csouthon44@gmail.com>
2023-04-16 12:40:38 +0100

    Creating contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,48 @@ import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+const [loading, setLoading] = useState(false);
+
+const handleChange = (e) => {}
+
+const handleSubmit = (e) => {}
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-row-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn('left', "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.heroSubText}>Get in touch</p>
+        <h3 className={styles.heroHeadText}>Contact Me.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">Name :</span>
+              <input 
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                placeholder="Enter your name"
+                className="bg-tertiary py-4 px-4 placeholder:text-secondary text-white first
+                rounded-lg outline-none border-none font-medium"
+              />
+          </label>
+        </form>
+      </motion.div>
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact");
