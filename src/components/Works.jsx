fe05c1cdf3d2485fa786134630608dbdde5ae089
CDS-admin1 <csouthon44@gmail.com>
2023-05-10 14:55:48 +0100

    Fixing ProjectCard on small devices

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -76,21 +76,21 @@ const Works = () => {
         <h2 className={`${styles.sectionHeadText}`}>Projects</h2>
       </motion.div>
 
-      <div className="w-full flex">
+      {/* <div className="w-full flex"> */}
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          {/* Here are my projects, showing my skills and experiences. Including live demo links and repository links. */}
-          Content coming soon...
+          Here are my projects, showing my skills and experiences. Including live demo links and repository links.
+          {/* Content coming soon... */}
         </motion.p>
-      </div>
+      {/* </div> */}
 
-      {/* <div className='mt-20 flex flex-wrap gap-7'>
+      <div className='mt-20 flex flex-wrap gap-7'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
-      </div> */}
+      </div>
     </>
   );
 };
