ca88ab846333387d73b18f507bac386bbbba5249
CDS-admin1 <csouthon44@gmail.com>
2023-05-15 10:13:14 +0100

    Fixing error with ProjectCard in sm device

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -25,7 +25,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className='bg-tertiary p-5 rounded-2xl sm:w-[300px] w-full'
       >
         <div className='relative w-full h-[230px]'>
           <img
@@ -79,17 +79,17 @@ const Works = () => {
       <p className={`${styles.sectionSubText}`}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects</h2>
       </motion.div>
-
+<div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Here are my projects, showing my skills and experiences. Including live demo links and repository links.
         </motion.p>
-
+        </div>
       <div className='mt-20 flex flex-wrap gap-7'>
         {projects.map((project, index) => (
-          <ProjectCard key={project.index} index={index} {...project} />
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
     </>
